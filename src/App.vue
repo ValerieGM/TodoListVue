0796a317844190e9b3dac00778e47186ed030f02
ValerieGM <vgongora@linkedstratergies.com>
2020-11-22 12:52:07 +0200

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,78 @@
+<template>
+  <div id="app">
+    <Header/>
+    <AddToDo v-on:add-todo="addTodo"/>
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteToDo"/>
+  </div>
+</template>
+
+<script>
+
+import Header from './components/layout/Header';
+import Todos from './components/Todos';
+import AddToDo from './components/AddTodo';
+
+export default {
+  name: 'app',
+  components:{
+    Header,
+    Todos,
+    AddToDo
+  },
+  data() {
+    return{
+      todos: [
+        {
+          id: 1,
+          title: "Todo One",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "Todo Two",
+          completed: true
+        },
+        {
+          id: 3,
+          title: "Todo Three",
+          completed: false
+        }
+      ]
+    }
+  },
+  methods: {
+    deleteToDo(id) {
+      this.todos = this.todos.filter(todo => todo.id != id);
+    },
+    addTodo(newTodo) {
+      this.todos = [...this.todos, newTodo];
+    }
+  }
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+}
+
+.btn {
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+
+.btn-hover {
+  background: #666;
+}
+</style>
